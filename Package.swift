12f8e5f4139e559a442649cb1722f7e802beebcf
Sͬeͥbͭaͭsͤtͬian <bastie@users.noreply.github.com>
2024-07-12 15:45:24 +0200

    move sources

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftFastPFOR",
+    platforms: [.macOS(.v13),.visionOS(.v1),.iOS(.v16),.tvOS(.v16)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SwiftFastPFOR",
+            targets: ["SwiftFastPFOR"]),
+    ],
+    dependencies: [
+      .package(
+        url: "https://github.com/bastie/JavApi4Swift.git",
+        .upToNextMajor(from: "0.8.1")
+      )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SwiftFastPFOR",
+            dependencies: [
+              .product(name: "JavApi", package: "JavApi4Swift")
+            ],
+            swiftSettings: [
+              .enableExperimentalFeature("StrictConcurrency")
+            ]
+        ),
+        .testTarget(
+            name: "SwiftFastPFORTests",
+            dependencies: ["SwiftFastPFOR",
+              .product(name: "iban4j2JavApi", package: "iban4j2JavApi")],                          
+        resources: [.process("Resources")]
+        ),
+    ]
+)
